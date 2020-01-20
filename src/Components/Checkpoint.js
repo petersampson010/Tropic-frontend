03dafda144e43a9341648a1f9bac9d5d794428c6
Peter Sampson <peter.sampson010@gmail.com>
2020-01-20 15:38:16 +0000

    color added to checkpoints

--- src/Components/Checkpoint.js
+++ src/Components/Checkpoint.js
@@ -10,9 +10,35 @@ export default class Checkpoint extends React.Component {
 
     distance = this.value*(this.containerWidth/this.props.maxVal)
 
+    colorAttr = (attr) => {
+        if (attr == "germinate") {
+            return "orange"
+        } else if (attr.includes("sprouting")) {
+            return "pink"
+        } else if (attr.includes("harvest")) {
+            return "brown"
+        } else if (attr.includes("maturity")) {
+            return "dark-green"
+        } else if (attr.includes("pot")) {
+            return "brown"
+        } else if (attr.includes("protect")) {
+            return "black"
+        } else if (attr.includes("flowering")) {
+            return "yellow"
+        } else if (attr.includes("fruiting")) {
+            return "red"
+        } else if (attr.includes("fertilize")) {
+            return 
+        } else if (attr.includes("bloom")) {
+            return "blue"
+        } else if (attr.includes("outside")) {
+            return "green"
+        }
+    }
+
     render() {
         return (
-            <div className="checkpoint" style={{left: this.distance}}>{this.key}</div>
+            <div className="checkpoint" style={{left: this.distance, color: this.colorAttr(this.key)}}>{this.key}</div>
         )
     }
 }
